c65d0b086a6ca9788749fc5592d56f8d2ccc48a5
Joel Ide <joel@Rebeccas-MacBook-Pro.local>
2022-01-29 14:20:48 +0900

    Add missin module to run in idea

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,10 @@
 module org.gnit.bible {
     requires com.github.ajalt.clikt;
     requires jdk.crypto.cryptoki;
+    requires ktor.http.jvm;
     requires ktor.client.okhttp.jvm;
     requires ktor.client.core.jvm;
     requires kotlin.stdlib;
+    requires kotlinx.coroutines.core.jvm;
     exports org.gnit.bible;
 }
