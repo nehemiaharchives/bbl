2ed6ff3fd69bb834a9d77453d153aea0e0d48764
Hokuto Ide <40375518+nehemiaharchives@users.noreply.github.com>
2022-10-22 11:25:53 +0900

    Chef recipe information was leftover so update it

--- bbl_install/metadata.rb
+++ bbl_install/metadata.rb
@@ -10,13 +10,8 @@ version          '0.3.0'
 
 supports 'ubuntu'
 
-#depends 'apt'
-
 # We use 13 chef but it should be compatible with 12.7
 chef_version '>= 13'
 
 # Provided recipes
-recipe 'mongodb-lib::mongo_gem', 'Installs mongo gem in compile time. Required for cluster operations'
-
-# Automatically installed gems before chef run (new in 12.8)
-# gem 'mongo', '>= 2.4.0'
+recipe 'bbl_install::default', 'Installs bbl package'
