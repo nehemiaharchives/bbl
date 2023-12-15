07622092d390a522484a1c3641747da61d50b460
Hokuto Ide <nehemiaharchive@gmail.com>
2023-12-15 20:45:34 +0900

    Update JDK21, Kotlin 1.9.21, gradle 8.5

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -14,7 +14,7 @@ module org.gnit.bible {
     requires org.apache.lucene.analysis.nori;
     requires org.apache.lucene.analysis.kuromoji;
     requires jimfs;
-    requires kotlin.stdlib.jdk7;
+    requires kotlin.stdlib.jdk8;
     requires jdk.management;
     requires com.sun.jna;
     requires org.apache.lucene.analysis.common;
