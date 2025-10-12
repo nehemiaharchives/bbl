6b505cc14472d8871c6c286fe57e3b66c6b15af4
Hokuto Joel Ide <nehemiaharchive@gmail.com>
2025-10-12 16:00:12 +0900

    AndroidPlatform.name is accessible without platformContext

--- shared/src/androidMain/kotlin/org/gnit/bible/Platform.android.kt
+++ shared/src/androidMain/kotlin/org/gnit/bible/Platform.android.kt
@@ -6,17 +6,21 @@ import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
 import java.io.File
 
-class AndroidPlatform(platformContext: Context) : Platform() {
+class AndroidPlatform(val platformContext: Any?) : Platform() {
     override val name: String = "Android ${Build.VERSION.SDK_INT}"
+
     override val packDir: String by lazy {
+        requireNotNull(platformContext){
+            "platformContext is required to get packDir"
+        }
+        require(platformContext is Context){
+            "platformContext must be a android.content.Context"
+        }
         File(platformContext.filesDir, "$bblDir/$packBaseDir").absolutePath
     }
 }
 
 actual fun getPlatform(platformContext: Any?): Platform{
-    require(platformContext is Context){
-        "platformContext must be a android.content.Context"
-    }
     return AndroidPlatform(platformContext)
 }
 
