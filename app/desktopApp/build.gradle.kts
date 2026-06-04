67d1f707cb370814e775f6466edea625ff5a0d41
Hokuto Joel Ide <nehemiaharchive@gmail.com>
2026-06-05 05:24:13 +0900

    Add desktopApp

---
+++ app/desktopApp/build.gradle.kts
@@ -0,0 +1,28 @@
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+
+plugins {
+    alias(libs.plugins.kotlinJvm)
+    alias(libs.plugins.composeMultiplatform)
+    alias(libs.plugins.composeCompiler)
+}
+
+dependencies {
+    implementation(projects.app.shared)
+
+    implementation(compose.desktop.currentOs)
+    implementation(libs.kotlinx.coroutinesSwing)
+
+    implementation(libs.compose.uiToolingPreview)
+}
+
+compose.desktop {
+    application {
+        mainClass = "org.gnit.bible.app.MainKt"
+
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "org.gnit.bible.app"
+            packageVersion = "1.0.0"
+        }
+    }
+}
---
+++ app/desktopApp/src/main/kotlin/org/gnit/bible/app/main.kt
@@ -0,0 +1,13 @@
+package org.gnit.bible.app
+
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "bbl",
+    ) {
+        App()
+    }
+}
