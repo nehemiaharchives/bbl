edb1086966f63162512f116a9852237d4d97c98e
Hokuto Joel Ide <nehemiaharchive@gmail.com>
2026-06-11 11:28:32 +0900

    Fix pack install E2E test code to capture proper bbl list result

--- bbl_install/test/integration/pack_install_posix/pack_install_uninstall_spec.rb
+++ bbl_install/test/integration/pack_install_posix/pack_install_uninstall_spec.rb
@@ -4,7 +4,7 @@ describe 'bbl pack install and uninstall' do
     expect(uninstall.exit_status).to eq(0)
     expect(uninstall.stdout).to include("Uninstalled kjv#{$bbl_eol}")
 
-    after_uninstall_list = command($bbl_run.call('list translations'))
+    after_uninstall_list = command("BBL_INSPEC_STEP=after_uninstall #{$bbl_run.call('list translations')}")
     expect(after_uninstall_list.exit_status).to eq(0)
     kjv_available = after_uninstall_list.stdout.lines.map(&:strip).find { |line| line.start_with?('KJV ') }
     expect(kjv_available).not_to be_nil
@@ -14,7 +14,7 @@ describe 'bbl pack install and uninstall' do
     expect(install.exit_status).to eq(0)
     expect(install.stdout).to include("Installed kjv#{$bbl_eol}")
 
-    after_install_list = command($bbl_run.call('list translations'))
+    after_install_list = command("BBL_INSPEC_STEP=after_install #{$bbl_run.call('list translations')}")
     expect(after_install_list.exit_status).to eq(0)
     kjv_installed = after_install_list.stdout.lines.map(&:strip).find { |line| line.start_with?('KJV ') }
     expect(kjv_installed).not_to be_nil
