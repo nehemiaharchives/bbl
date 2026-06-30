4cf156328d5b5dbfb9f548374a4f82eca94ad4c5
Hokuto Joel Ide <nehemiaharchive@gmail.com>
2026-06-30 19:45:45 +0900

    app: Feed LocalContext to App() in MainActivity

--- app/androidApp/src/main/kotlin/org/gnit/bible/app/MainActivity.kt
+++ app/androidApp/src/main/kotlin/org/gnit/bible/app/MainActivity.kt
@@ -1,17 +1,19 @@
 package org.gnit.bible.app
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.ui.platform.LocalContext
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
-
         setContent {
-            App()
+            val context: Context = LocalContext.current
+            App(platformContext = context)
         }
     }
 }
